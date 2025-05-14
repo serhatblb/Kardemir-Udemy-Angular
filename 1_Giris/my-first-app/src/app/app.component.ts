ae0a5e779930b5d5f95a42abdf2aeccb8e7f8805
serhatblb <serhatblb23@gmail.com>
2025-05-14 17:03:01 +0300

    componentler eklendi

--- 1_Giris/my-first-app/src/app/app.component.ts
+++ 1_Giris/my-first-app/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,AboutComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
