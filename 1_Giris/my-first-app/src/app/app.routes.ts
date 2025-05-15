15b31b97790ff86086480c57777b3336dbc60569
serhatblb <serhatblb23@gmail.com>
2025-05-15 17:29:34 +0300

    değişiklikler yapildi

--- 1_Giris/my-first-app/src/app/app.routes.ts
+++ 1_Giris/my-first-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
