c6b8c8f3c843cf88caae32ae3b9f33d5152fe0ce
serhatblb <serhatblb23@gmail.com>
2025-05-21 16:20:44 +0300

    two data binding ornegi eklendi

--- 1_Giris/my-first-app/src/app/app.component.ts
+++ 1_Giris/my-first-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
